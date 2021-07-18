b747814a660684be5b2af9a8c89f345018057db0
Salocin11 <s10206093@connect.np.edu.sg>
2021-07-19 00:04:03 +0800

    Added a Search Feature for Articles page

--- Philip/Pages/Articles/Index.cshtml.cs
+++ Philip/Pages/Articles/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Philip.Data;
 using Philip.Models;
 
@@ -21,10 +22,21 @@ namespace Philip.Pages.Articles
         }
 
         public IList<Article> Article { get;set; }
-
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Title { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string ArticleTitle { get; set; }
         public async Task OnGetAsync()
         {
-            Article = await _context.Article.ToListAsync();
+            var articles = from m in _context.Article
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                articles = articles.Where(s => s.Title.Contains(SearchString));
+            }
+
+            Article = await articles.ToListAsync();
         }
     }
 }
