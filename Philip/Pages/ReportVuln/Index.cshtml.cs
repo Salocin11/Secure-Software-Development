703915783c1b0efebb30e19b5e4ff34eab704c03
Salocin11 <s10206093@connect.np.edu.sg>
2021-07-27 05:05:05 +0800

    Added Misc page (Report vuln)  in the form of a feedback model. Added authentication requirements for article and feedback models. Added an alert for submission.

---
+++ Philip/Pages/ReportVuln/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Philip.Data;
+using Philip.Models;
+
+namespace Philip.Pages.ReportVuln
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Philip.Data.PhilipContext _context;
+
+        public IndexModel(Philip.Data.PhilipContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Feedback> Feedback { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Feedback = await _context.Feedback.ToListAsync();
+        }
+    }
+}
