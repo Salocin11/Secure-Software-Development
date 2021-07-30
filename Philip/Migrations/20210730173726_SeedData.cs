5d02db39820c6c89f83d304b66964b5153b8cf10
Nicholas Ian Boey <s10203273@connect.np.edu.sg>
2021-07-31 02:02:19 +0800

    Added design for create article button

---
+++ Philip/Migrations/20210730173726_SeedData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Philip.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IPAddress",
+                table: "AspNetRoles");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IPAddress",
+                table: "AspNetRoles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
